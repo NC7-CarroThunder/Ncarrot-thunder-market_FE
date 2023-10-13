a69bf0a8477ee585aeb32b8ced1ae499abfc4185
bibibigg <aerl0706@naver.com>
2023-10-13 19:54:47 +0900

    feat:게시글 리스트 이미지 불러오기

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
-import { Link } from 'react-router-dom'; // Link 컴포넌트 import
+import { Link } from 'react-router-dom';
 import ROUTER from '../constants/router';
 
 export default function MainPage() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [selectedCategory, setSelectedCategory] = useState('전체'); // 선택된 카테고리
+  const [selectedCategory, setSelectedCategory] = useState('전체');
 
   useEffect(() => {
     async function fetchPosts() {
@@ -36,15 +36,29 @@ export default function MainPage() {
     setSelectedCategory('전체');
   };
 
+// 게시글 등록시 카테고리선택이 구현이 되면 setSelectedCategory에 카테고리 명을 적으면 됩니다
+
   return (
     <MainWrapper>
       <CategoryTitle>전체 카테고리</CategoryTitle>
       <CategoryButtonRow>
         <div>
-          <CategoryButton onClick={() => setSelectedCategory('DIGITAL')}>디지털 기기</CategoryButton>
-          <CategoryButton onClick={() => setSelectedCategory('BOOK')}>도서</CategoryButton>
-          <CategoryButton onClick={() => setSelectedCategory('ETC')}>기타</CategoryButton>
-          <CategoryButton onClick={showAllPosts}>전체 게시글</CategoryButton>
+    <CategoryButton onClick={showAllPosts}>전체 게시글</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('DIGITAL')}>디지털 기기</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('가구/인테리어')}>가구/인테리어</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('의류')}>의류</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('생활가전')}>생활가전</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('생활/주방')}>생활/주방</CategoryButton>
+    </div>
+    </CategoryButtonRow>
+    <CategoryButtonRow>
+    <div>
+    <CategoryButton onClick={() => setSelectedCategory('스포츠/레저')}>스포츠/레저</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('자동차/공구')}>자동차/공구</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('BOOK')}>도서</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('뷰티/미용')}>뷰티/미용</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('반려동물용품')}>반려동물용품</CategoryButton>
+    <CategoryButton onClick={() => setSelectedCategory('ETC')}>기타</CategoryButton>
         </div>
       </CategoryButtonRow>
       <StyledImage src="/vintage-collection.jpg" alt="Vintage Collection" />
@@ -57,13 +71,13 @@ export default function MainPage() {
               <Col key={post.postid} md={3}>
                 <Card>
                   <CardImg
-                    src={post.imageUrl}
+                    src={`https://kr.object.ncloudstorage.com/carrot-thunder/article/${post.attachedFilesPaths[0]}`}
                     alt={'게시글 이미지'}
                     className="card-img-top"
                   />
                   <CardBody>
-                  <Link to={`/post/${post.postid}`}>
-                    <CardTitle>{post.title}</CardTitle>
+                    <Link to={`/post/${post.postid}`}>
+                      <CardTitle>{post.title}</CardTitle>
                     </Link>
                     <CardText>Price: {post.price}</CardText>
                     <CardText>Address: {post.address}</CardText>
@@ -79,6 +93,7 @@ export default function MainPage() {
 }
 
 const MainWrapper = styled.main`
+  max-width: 1440px;
   width: 100%;
   height: 100%;
   overflow-y: auto;
@@ -87,6 +102,7 @@ const MainWrapper = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin: 0 auto; 
 `;
 
 const CategoryTitle = styled.h1`
@@ -111,7 +127,7 @@ const StyledImage = styled.img`
 `;
 
 const CategoryButton = styled.button`
-  width: 6rem;
+  width: 7rem;
   font-size: 13px;
   padding: 10px;
   background-color: #ff922b;
